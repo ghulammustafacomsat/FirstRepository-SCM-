5f4df4ac6a12b03679157a68f80ac4160ad6a9b9
ghulammustafacomsat <ghulammustafa092@gmail.com>
2016-04-25 17:41:14 +0500

    Your first commit

---
+++ SCM/src/MainClass.java
@@ -0,0 +1,12 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Hello World");
+	System.out.println("Mustafa");
+	System.out.println("sp13-bse-092");
+	}
+
+}
